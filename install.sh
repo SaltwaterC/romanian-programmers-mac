49903f4673d100db18a97d3319fac4f533fd33c2
SaltwaterC <saltwaterc@gmail.com>
2018-03-09 01:48:47 +0000

    Change install.sh to point to this branch.

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "Acele aranjamente sunt de fapt vreo doua din punct de vedere functional."
 echo
 echo "» Primul aranjament are tasta inscriptionata cu \"\` si ~\" inainte de 1"
 echo "» Al doilea aranjament are tasta inscriptionata cu \"§ si ±\" inainte de 1"
-echo 
+echo
 echo "Cel de-al doilea aranjament se poate intalni si in cazul tastaturilor care"
 echo "au inscriptionate diacritice pentru limba Romana fabricate de Apple, dar "
 echo "nu in pozitiile definite de SR 13392:2004. Acest tip de aranjament are o "
@@ -43,11 +43,11 @@ else
 	echo "» Instalez Romanian - Programmers folosind GitHub"
 	echo "» Instalez Romanian - Programmers.icns"
 	sudo curl --silent --location --max-redirs 10 \
-"https://raw.githubusercontent.com/SaltwaterC/romanian-programmers-mac/master/Romanian%20-%20Programmers.icns" \
+"https://raw.githubusercontent.com/SaltwaterC/romanian-programmers-mac/non-standard/uk/Romanian%20-%20Programmers.icns" \
 --output "/Library/Keyboard Layouts/Romanian - Programmers.icns"
 	echo "» Instalez Romanian - Programmers ${layout}.keylayout"
 	sudo curl --silent --location --max-redirs 10 \
-"https://raw.githubusercontent.com/SaltwaterC/romanian-programmers-mac/master/Romanian%20-%20Programmers%20${layout}.keylayout" \
+"https://raw.githubusercontent.com/SaltwaterC/romanian-programmers-mac/non-standard/uk/Romanian%20-%20Programmers%20${layout}.keylayout" \
 --output "/Library/Keyboard Layouts/Romanian - Programmers.keylayout"
 fi
 
